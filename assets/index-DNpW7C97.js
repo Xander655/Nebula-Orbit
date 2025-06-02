22ade180efac581b112a1738bdc061d75f2a66de
Xander655 <Xander655@users.noreply.github.com>
2025-06-02 20:13:56 +0000

    deploy: 3a2cf11533acb9d0dc75fe99114e543a35712b6b

--- assets/index-DNpW7C97.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-DfQz9cF2.js";const e=function(){return o.jsx("div",{children:'Hello "/admin/timeoff"!'})};export{e as component};
---
+++ assets/index-mejYsByN.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-DVynMCWE.js";const e=function(){return o.jsx("div",{children:'Hello "/admin/timeoff"!'})};export{e as component};
